0d2d7fe6e07f38bb7c1770ca2d462f698c706a2c
pmdmdam2 <rmenmar264@gmail.com>
2019-10-31 12:56:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/scrollviewtest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scrollviewtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
